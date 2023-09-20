93f2302eba897dc316ba1231bae49aaacce6e07c
noveky <65780673+noveky@users.noreply.github.com>
2023-09-20 16:51:59 +0800

    Upload Assignment 2

---
+++ 第二次作业/Assignment2/Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+	class Account
+	{
+		public string Id
+		{
+			get => id;
+			set
+			{
+				if (value.Length != 8) throw new Exception("账号只能为8位");
+				foreach (char ch in value)
+				{
+					if (!char.IsDigit(ch)) throw new Exception("账号只能为数字");
+				}
+				id = value; 
+			}
+		}
+		public string Passcode
+		{
+			get => passcode;
+			set
+			{
+				if (value.Length != 6) throw new Exception("密码只能为6位");
+				foreach (char ch in value)
+				{
+					if (!char.IsDigit(ch)) throw new Exception("密码只能为数字");
+				}
+				passcode = value;
+			}
+		}
+
+		string id = string.Empty;
+		string passcode = string.Empty;
+
+		public decimal Balance { get; protected set; }
+		
+		public Account(string id)
+		{
+			Id = id;
+			Passcode = "000000";
+		}
+
+		public void Deposit(decimal amount)
+		{
+			Balance += amount;
+		}
+
+		public virtual void Withdraw(decimal amount)
+		{
+			if (Balance - amount < 0)
+			{
+				throw new Exception("余额不足，取款失败");
+			}
+			Balance -= amount;
+		}
+	}
+}
---
+++ 第二次作业/Assignment2/CreditAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+	class CreditAccount : Account
+	{
+		public decimal CreditLimit { get; set; }
+
+		public CreditAccount(string id, decimal creditLimit) : base(id)
+		{
+			CreditLimit = creditLimit;
+		}
+
+		public override void Withdraw(decimal amount)
+		{
+			if (Balance - amount < -CreditLimit)
+			{
+				throw new Exception("超出信用额度，取款失败");
+			}
+			Balance -= amount;
+		}
+	}
+}
